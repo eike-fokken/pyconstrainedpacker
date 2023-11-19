6458b939fd16880e35be2c1937e25a1b5904a97f
Eike Fokken <e.fokken+github@posteo.de>
2023-11-19 22:38:11 +0100

    added my playground file

---
+++ tests/playground.py
@@ -0,0 +1,27 @@
+# d = dict()
+
+# d[5.3] = 9
+# d[2.3] = 4
+
+# print("presort:")
+# for key, value in d.items():
+#     print(f"{key}: {value}")
+
+# print("postsort:")
+# for key, value in sorted(d.items()):
+#     print(f"{key}: {value}")
+
+# print(d)
+import casadi
+import numpy as np
+
+x = casadi.MX.sym("x")
+
+y = casadi.MX.sym("y", 2)
+
+mylist = [x, y]
+
+
+final_casadi_variable = casadi.vertcat(*mylist)
+
+print(final_casadi_variable.size())
